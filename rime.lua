c081684c17aa1ad9ef0c318c0754cc2d6989da41
Xuan MA <SuperMMX@gmail.com>
2023-01-28 09:39:25 +1100

    Add 徐码

---
+++ rime.lua
@@ -0,0 +1,8 @@
+-- rime.lua
+
+forfudan_freq_filter = require("forfudan_freq_filter")
+forfudan_freq_first = forfudan_freq_filter.forfudan_freq_first
+forfudan_freq_only = forfudan_freq_filter.forfudan_freq_only
+
+-- 全碼後置和全碼出單
+forfudan_single_char_only_for_full_code = require("forfudan_single_char_only_for_full_code")
